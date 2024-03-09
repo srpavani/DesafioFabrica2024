2cfa97cbdd6d5f10f66364d56a73d0ed2fbc078d
diogopavani <pavanidiogo1@gmail.com>
2024-03-09 09:53:41 -0300

    :sparkles: adicionei o msql

--- projeto/settings.py
+++ projeto/settings.py
@@ -77,12 +77,15 @@ WSGI_APPLICATION = 'projeto.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "alimentos",
+        "USER": "root",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
