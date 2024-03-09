4473d484a8b258a3402367818cd18d77ce91e1d2
diogopavani <53492119+srpavani@users.noreply.github.com>
2024-03-09 20:42:01 -0300

    :bug: Coloquei o sqlite como bd default e mysql como secundario
    Se nao tiver um servidor mysql vai funcionar tranquilo

--- projeto/settings.py
+++ projeto/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'projeto.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
+    "msql": {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "alimentos",
         "USER": "root",
@@ -85,7 +85,7 @@ DATABASES = {
         "HOST": "localhost",
         "PORT": "3306",
     },
-    "sqlite": {
+    "default": {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3'
     },
